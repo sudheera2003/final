0124de250de857c278b67b08281dd895ad8be45f
sudheera2003 <sudheeradilum@gmail.com>
2026-02-18 20:34:56 +0530

    changes for render

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,14 @@
-from gevent import monkey
-monkey.patch_all()
+import eventlet
+eventlet.monkey_patch()
 
 import os
 from app import create_app, socketio
 
+# Initialize the app
 app = create_app()
 
 if __name__ == '__main__':
+    # LOCAL RUN LOGIC
     port = int(os.environ.get("PORT", 5000))
-    print(f"🚀 Server starting on port {port}...")
+    print(f"Local Server starting on port {port}...")
     socketio.run(app, host='0.0.0.0', port=port, debug=True, use_reloader=False)
